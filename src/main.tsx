535ba03f2d58fe600faa76914c7064cfc820b795
Fabian Gonzalez <andersonfabygonzalez.21@gmail.com>
2026-05-11 10:05:41 -0500

    Refactor mocking initialization in main.tsx for improved readability

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Providers } from './app/providers';
+import { initMocks } from './mocks/init';
 
-async function enableMocking() {
-  if (import.meta.env.DEV && import.meta.env.VITE_MSW_ENABLED === 'true') {
-    const { worker } = await import('./mocks/browser');
-    return worker.start();
-  }
-}
-
-enableMocking().then(() => {
+initMocks().then(() => {
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <Providers />
